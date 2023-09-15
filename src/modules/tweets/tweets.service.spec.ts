d45af6affe7ed52937038becf50b87f9fb36db44
manishc <manishcarpenter15@gmail.com>
2023-09-15 13:51:53 +0530

    Unit testing for tweet service is done

--- src/modules/tweets/tweets.service.spec.ts
+++ src/modules/tweets/tweets.service.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TweetsService } from './tweets.service';
+import exp from 'constants';
+import { platform } from 'os';
 
 describe('TweetsService', () => {
   let service: TweetsService;
@@ -40,4 +42,104 @@ describe('TweetsService', () => {
       expect(tweet).toThrowError();
     });
   });
+
+  describe(`updatedTweet`, () => {
+    it(`should prevent update and throw error, tweet index does not exist`, () => {
+      // Arrange
+      service.tweets = [];
+      const payload = {
+        tweet: `First Tweet`,
+        id: 1
+      }
+
+      // Act
+      const tweet = () => {
+        return service.updateTweet(payload.tweet, payload.id)
+      }
+
+      // Assert
+      expect(tweet).toThrowError();
+      
+    })
+
+    it(`should prevent the update tweet if tweet length is greater then 100`, () => {
+      // Arrage
+      service.tweets = ['Dummy twee'];
+      const tweet = `This is going to be a very long tweet. A tweet which contain more then 100 character at least. You can check it`;
+
+      const payload = {
+        tweet: tweet,
+        id: 0
+      }
+
+      // Act
+      const response = () => { 
+        return service.updateTweet(payload.tweet, payload.id)
+      }
+
+      // Assert
+      expect(response).toThrowError(`Tweet too long`)
+    })
+
+    it(`Should update the tweet and return updated tweet`, () => {
+      // Arrage
+      service.tweets = ['Hi, its older tweet'];
+      const payload = {
+        tweet: `Hi, its latest tweet`,
+        id: 0
+      }
+
+      // Act 
+      const tweet = service.updateTweet(payload.tweet, payload.id);
+
+      // Assert
+      expect(tweet).toBe(payload.tweet)
+      expect(service.tweets[payload.id]).toBe(payload.tweet)
+      expect(service.tweets).toHaveLength(1)
+
+    })
+  });
+
+  describe(`getTweet`, () => {
+  it(`Should return tweet array`, () => {
+    // Arrage
+    service.tweets = [`Tweet 1`, `Tweet 2`, `Tweet 3`];
+
+    // Act
+    const tweets = service.getTweets()
+
+    // Assert
+    expect(tweets).toBe(service.tweets)
+    expect(tweets).toHaveLength(3)
+  })
+
+  describe(`deleteTweet`, () => {
+    it(`Should throw error if tweet does not exist on particular indext`, () => {
+      // Arrage
+      service.tweets = [];
+      const payload = 1
+
+      // Act
+      const tweet = () => {
+        return service.deleteTweet(payload)
+      }
+
+      // Assert
+      expect(tweet).toThrowError('This Tweet does not exist')
+    })
+
+    it(`Should delete tweet`, () => {
+      // Arrage
+      service.tweets = [`Tweet 1`]
+      const payload = 0;
+
+      // Act
+      const deletedTweet = service.deleteTweet(payload)
+
+      // Assert
+      expect(deletedTweet).toBe(`Tweet 1`)
+    })
+  })
+
+  })
 });
