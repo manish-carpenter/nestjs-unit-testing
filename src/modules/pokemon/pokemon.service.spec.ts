3beea2556e745776ca73affa54c7c0c18f53a977
manishc <manishcarpenter15@gmail.com>
2023-09-18 11:58:08 +0530

    mocking http service

--- src/modules/pokemon/pokemon.service.spec.ts
+++ src/modules/pokemon/pokemon.service.spec.ts
@@ -1,18 +1,66 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PokemonService } from './pokemon.service';
+import { HttpService } from '@nestjs/axios';
+import { DeepMocked, createMock } from '@golevelup/ts-jest';
+import { BadRequestException } from '@nestjs/common';
+
 
 describe('PokemonService', () => {
-  let service: PokemonService;
+  let pokemonService: PokemonService;
+  let httpService: DeepMocked<HttpService>
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PokemonService],
-    }).compile();
+      providers: [PokemonService]
+    }).useMocker(createMock).compile();
 
-    service = module.get<PokemonService>(PokemonService);
+    pokemonService = module.get<PokemonService>(PokemonService);
+    httpService = module.get(HttpService);
   });
 
-  // it('should be defined', () => {
-  //   expect(service).toBeDefined();
-  // });
+  describe('getPokemon', () => {
+    it('should throw error if Id is less than 1', async () => {
+      // Arrage
+      const payload = 0
+  
+      // Act
+      const response = pokemonService.getPokemon(payload);
+  
+      // Assert
+      await expect(response).rejects.toBeInstanceOf(BadRequestException);
+
+    });
+
+    it('should throw error if Id is greater than 151', async () => {
+      // Arrage
+      const payload = 152
+  
+      // Act
+      const response = pokemonService.getPokemon(payload);
+  
+      // Assert
+      await expect(response).rejects.toBeInstanceOf(BadRequestException);
+
+    });
+
+    it('valid pokemon Id to return the pokemon', async () => {
+      // Arrage
+      httpService.axiosRef.mockResolvedValueOnce({
+        data: {
+          species: { name: `bulbasaur` },
+        },
+        headers: {},
+        config: { url: ``},
+        status: 200,
+        statusText: ``,
+      });
+      const payload = 1;
+      
+      // Act
+      const getPokemon = pokemonService.getPokemon(payload);
+      
+      // Assert
+      await expect(getPokemon).resolves.toBe('bulbasaur');
+    });
+  })
 });
